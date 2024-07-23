ea1ecd3e6d95ab85ebad05b5f6df59b34584b0de
purple-slingshot <hello@purpleslingshot.com>
2024-07-23 15:43:44 +0530

    Fix line length issue in regression_model.py

--- model/regression_model.py
+++ model/regression_model.py
@@ -11,7 +11,9 @@ X = data.drop('MEDV', axis=1)
 y = data['MEDV']
 
 # Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Train the model
 model = LinearRegression()
